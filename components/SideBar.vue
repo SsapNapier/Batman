f70817d40004a3333adfd45319129598a684ed1b
SsapNapier <48889205+SsapNapier@users.noreply.github.com>
2019-04-01 20:38:17 +0300

    Add files via upload

---
+++ components/SideBar.vue
@@ -0,0 +1,63 @@
+<template lang="jade">
+div.sidebar
+  a.active.main Войти&nbsp;&nbsp;&nbsp;&#8594;
+  div.bar
+    a {{ greeting }}
+    hr
+    a Ж/Д Авиабилеты
+    hr
+    a Гостиницы
+    hr
+    a Страхование
+    hr
+    a Трансфер
+</template>
+
+<script>
+    module.exports = {
+        data: function() {
+            return {
+                greeting: 'Авиабилеты'
+            }
+        }
+    }
+</script>
+
+<style lang="sass?indentedSyntax=true">
+.sidebar
+  font-family: SegoeUI
+  font-size: 12px
+  flex-basis: 254px
+  border-left: 1px solid #c5c5c5
+  height: 966px
+  align-items: flex-end
+
+hr
+  position: relative
+  top: 89px
+  left: 45px
+  margin-left: 0
+  width: 5%
+  margin-bottom: 15%
+
+
+.bar
+  position: relative
+  top: 171px
+
+a
+  position: relative
+  top: 92px
+  left: 45px
+  color: #c5c5c5
+  display: flex
+  flex-direction: column
+  padding-bottom: 14%
+
+  &.active
+    color: #00aeef
+    padding-bottom: 14px
+    text-decoration: none
+    cursor: pointer
+
+</style>
